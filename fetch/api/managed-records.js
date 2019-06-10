b7043b642c0966a360304aa10676faab07f91c7f
Tenor Biel <tenorbiel@gmail.com>
2019-06-10 08:20:57 -0400

    dat commit tho

--- fetch/api/managed-records.js
+++ fetch/api/managed-records.js
@@ -4,6 +4,74 @@ import URI from "urijs";
 // /records endpoint
 window.path = "http://localhost:3000/records";
 
-// Your retrieve function plus any additional functions go here ...
+const pageSize = 10
+
+async function retrieve (options = {}) {
+  const {
+    page = 1,
+    colors = []
+  } = options
+
+  const url = constructRequestURL(page, colors)
+
+  const response = await fetch(url)
+
+  let body = []
+
+  try {
+    body = await response.json()
+  } catch (error) {
+    console.log('Request failed', error)
+  }
+
+  const pageResults = body.slice(0, 10)
+  const nextPageResults = body.slice(10)
+
+  const ids = pageResults.map(({id}) => id)
+
+  const open = pageResults.filter(isOpen).map(decorateRecord)
+
+  const closedPrimaryCount = pageResults.filter(
+    (record) => isClosed(record) && isPrimary(record.color)
+  ).length
+
+  return {
+    previousPage: page === 1 ? null : page - 1,
+    nextPage: nextPageResults.length === 0 ? null : page + 1,
+    ids,
+    open,
+    closedPrimaryCount
+  }
+}
 
 export default retrieve;
+
+function isOpen (record) {
+  return record.disposition === 'open'
+}
+
+function isClosed (record) {
+  return record.disposition === 'closed'
+}
+
+function isPrimary (color) {
+  return ['red', 'yellow', 'blue'].includes(color)
+}
+
+function decorateRecord (record) {
+  return Object.assign({}, record, {
+    isPrimary: isPrimary(record.color)
+  })
+}
+
+function constructRequestURL (page, colors) {
+  return URI(window.path).search({
+    // back-end only accepts single-member arrays when we use this key
+    'color[]': colors,
+
+    // we peek at first record of next page to determine existence
+    limit: pageSize + 1,
+
+    offset: (page - 1) * pageSize
+  })
+}
